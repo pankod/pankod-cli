ad8461aff2e7e19fd56f9794f6171ce5932bb9ae
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-07-19 13:29:16 +0300

    Generate packageJson mock

--- test/setup.ts
+++ test/setup.ts
@@ -13,4 +13,7 @@ fs.writeFileSync('/src/Templates/index.ts', '');
 fs.writeFileSync('/src/Templates/moleculer/moleculer.mustache', 'Test template moleculer.mustache');
 fs.writeFileSync('/src/Templates/nextjs/nextjs.mustache', 'Test template nextjs.mustache');
 
+// Create Package.json
+fs.writeFileSync('/package.json', `{ "pankod": { "projectType": "test", "plugins": ["test"] } }`);
+
 jest.mock('fs', () => require('memfs'));
