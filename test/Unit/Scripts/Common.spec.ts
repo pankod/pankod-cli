f5eaab774e4681b193167ea5cc95f574f8f6826f
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-07-19 13:11:59 +0300

    Add addToIndex spec :tada:

--- test/Unit/Scripts/Common.spec.ts
+++ test/Unit/Scripts/Common.spec.ts
@@ -1,4 +1,22 @@
+import { fs } from 'memfs';
+import { CommonHelper } from '../../../src/Scripts/Common';
+import { ICommon } from '../../../src/Scripts/ICommon';
 
-it('should be true', () => {
-	expect(true).toBe(true);
-})
+describe('Common Helper', () => {
+	describe('addToIndex', () => {
+		it('should add index file', async () => {
+			const fileContent = `export { test } from 'test'`;
+			const addToIndexParams: ICommon.IAddIndex = {
+				dirPath: '/src/Templates/index.ts',
+				getFileContent: () => fileContent,
+				message: 'Test index added'
+			};
+
+			CommonHelper.addToIndex(addToIndexParams);
+
+			const addedIndex = fs.readFileSync('/src/Templates/index.ts');
+
+			expect(String(addedIndex)).toEqual(`${fileContent}\n`);
+		});
+	});
+});
