c9a8dee44e2fe758259206e145fef467848be687
aydın akyol <aydinakyol11@gmail.com>
2019-07-24 15:17:27 +0300

    createTest test and  setupjs edited

--- test/Unit/Scripts/moleculer/helper.spec.ts
+++ test/Unit/Scripts/moleculer/helper.spec.ts
@@ -1,17 +1,27 @@
-import { IMoleculerHelper } from './../../../../src/Scripts/moleculer/IMoleculerTypes.d';
+import { Answers } from 'inquirer';
 import { fs } from 'memfs';
 import { CommonHelper } from '../../../../src/Scripts/Common';
 import { Helper } from '../../../../src/Scripts/moleculer/helper';
 import { Config } from '../../../../src/config';
+import { IMoleculerHelper } from './../../../../src/Scripts/moleculer/IMoleculerTypes.d';
+import { ICommon } from '../../../../src/Scripts/ICommon';
 
 describe('Helper tests', () => {
+
+
 	it('should addBrokerHelper', () => {
+		// const brokerHelperTemplatesParams: IMoleculerHelper.IBrokerHelperTemplatesParams = {
+		// 	brokerHelperCreate: Config.moleculer.templates.brokerHelperCreate,
+		// 	brokerHelperImport: Config.moleculer.templates.brokerHelperImport,
+		// 	replaceFileDir: Config.moleculer.brokerHelper
+		// };
+
 		const brokerHelperTemplatesParams: IMoleculerHelper.IBrokerHelperTemplatesParams = {
 			brokerHelperCreate: '/Templates/moleculer/Tests/BrokerHelperCreate.mustache',
 			brokerHelperImport: '/Templates/moleculer/Tests/BrokerHelperImport.mustache',
 			replaceFileDir: '/test/Utils/BrokerHelper.ts'
 		};
-		const answers = {
+		const answers: ICommon.IAnswers = {
 			fileName: 'tests',
 			lowerFileName: 'tests',
 			upperFileName: 'Tests'
@@ -26,13 +36,19 @@ describe('Helper tests', () => {
 	});
 
 	it('should createEntityInstance', () => {
+		// const createEntityTemplatesParams = {
+		// 	indexTemplate: Config.moleculer.templates.createEntityIndexTemplate,
+		// 	templatePath: Config.moleculer.templates.createEntityTemplatePath
+		// };
+
+
+		const answers: ICommon.IAnswers = {
+			fileName: 'tests'
+		};
+
 		const createEntityTemplatesParams = {
 			indexTemplate: '/Templates/moleculer/Repositories/EntityIndex.mustache',
 			templatePath: '/Templates/moleculer/Repositories/Entity.mustache'
-		};
-
-		const answers = {
-			fileName: 'tests'
 		};
 
 		const fileIndexContentPath = `${Config.moleculer.entityDir}/index.ts`;
@@ -48,4 +64,63 @@ describe('Helper tests', () => {
 		expect(fileIndexContent).toBe(CommonHelper.getTemplate(createEntityTemplatesParams.indexTemplate, answers) + '\n');
 
 	});
+	// it('should createRepository', () => {
+	// 	const answers: ICommon.IAnswers = {
+	// 		fileName: '',
+	// 		upperFileName: 'Test'
+	// 	};
+
+	// 	const createRepositoryTestTemplateParams = {
+	// 		indexTemplate: '/Templates/moleculer/Repositories/RepoIndex.mustache',
+	// 		templatePath: '/Templates/moleculer/Repositories/Repository.mustache'
+	// 	};
+
+	// 	const createTestRepositoryTemplatePath = '/Templates/moleculer/Tests/Repository.mustache';
+	// 	console.log('createRepository');
+
+	// 	Helper.createRepository(answers, createRepositoryTestTemplateParams, createTestRepositoryTemplatePath);
+
+	// 	const fileContentPath = `${Config.moleculer.repositoriesDir}/${answers.upperFileName}.ts`;
+	// 	const fileContent = String(fs.readFileSync(fileContentPath));
+
+	// 	expect(fileContent).toBe(CommonHelper.getTemplate(createRepositoryTestTemplateParams.templatePath, answers));
+
+	// 	console.log('fileContent', fileContent);
+
+	// 	console.log('fileContent', CommonHelper.getTemplate(createRepositoryTestTemplateParams.templatePath, answers));
+
+	// });
+
+	it('should createTest', () => {
+		const answers: ICommon.IAnswers = {
+			fileName: 'service',
+			hasDatabase: true,
+			isPrivate: true,
+			lowerFileName: 'service',
+			upperFileName: 'Service'
+		};
+		const templateProps = {
+			fileName: answers.fileName,
+			hasDatabase: answers.hasDatabase,
+			isPrivate: answers.isPrivate,
+			lowerFileName: answers.lowerFileName,
+			upperFileName: answers.upperFileName
+		};
+		const options: ICommon.ICreateTest = {
+			answers,
+			dirPath: `${Config.moleculer.serviceHelperTestDir}/${answers.upperFileName}Helper.spec.ts`,
+			successMessage: 'Added new Microservice test.',
+			templatePath: '/Templates/moleculer/Tests/ServiceHelper.mustache',
+			templateProps
+		};
+
+		Helper.createTest(options);
+
+		const fileContentPath = `${Config.moleculer.serviceHelperTestDir}/${answers.upperFileName}Helper.spec.ts`;
+		const fileContent = String(fs.readFileSync(fileContentPath));
+
+		expect(fileContent).toBe(CommonHelper.getTemplate(options.templatePath, answers));
+
+	});
+
 });
--- test/setup.ts
+++ test/setup.ts
@@ -9,6 +9,11 @@ fs.mkdirSync('/src/Templates/nextjs');
 
 fs.mkdirSync('/src/Entities');
 
+fs.mkdirSync('/src/Interfaces');
+fs.mkdirSync('/src/Interfaces/Repositories');
+fs.mkdirSync('/src/Interfaces/Repositories/Test');
+
+
 fs.mkdirSync('/app');
 fs.mkdirSync('/app/pages');
 fs.mkdirSync('/app/pages/test');
@@ -18,6 +23,8 @@ fs.mkdirSync('/app/Test');
 
 fs.mkdirSync('/test');
 fs.mkdirSync('/test/Utils');
+fs.mkdirSync('/test/Unit');
+fs.mkdirSync('/test/Unit/ServiceHelpers');
 
 fs.mkdirSync('/Templates');
 fs.mkdirSync('/Templates/moleculer');
@@ -54,6 +61,64 @@ export class {{fileName}} {
 `);
 
 fs.writeFileSync('/Templates/moleculer/Repositories/EntityIndex.mustache', `export { {{fileName}} } from './{{fileName}}';`);
+
+fs.writeFileSync('/Templates/moleculer/Tests/Repository.mustache', `import { {{upperFileName}}Repository } from '../../../src/Repositories/{{upperFileName}}';
+
+describe('Test {{upperFileName}}Repository constructor', () => {
+	it('should create an empty options', () => {
+		expect({{upperFileName}}Repository).toBeDefined();
+	});
+
+	it('should work (method name)', async () => {
+		const result = await {{upperFileName}}Repository.methodName(1);
+		expect(result).toBeDefined();
+	});
+});
+`);
+
+fs.writeFileSync('/Templates/moleculer/Repositories/RepoIndex.mustache', `export { {{upperFileName}}Repository } from '@Repositories/{{upperFileName}}';`);
+fs.writeFileSync('/Templates/moleculer/Repositories/Repository.mustache', `//#region Local Imports
+import { {{upperFileName}} } from '@Entities/{{upperFileName}}';
+//#endregion Local Imports
+
+//#region Interface Imports
+import { I{{upperFileName}} } from '@Interfaces';
+//#endregion Interface Imports
+
+export namespace {{upperFileName}}Repository {
+	export const exampleMethod = async (): Promise<I{{fileName}}> => {
+
+		return;
+	};
+}
+`);
+
+fs.writeFileSync('/test/Unit/ServiceHelper.spec.ts', '');
+fs.writeFileSync('/Templates/moleculer/Tests/ServiceHelper.mustache', `//#region Local Imports
+import { {{upperFileName}}Helper } from '@ServiceHelpers';
+import { DummyContext } from '@Test/Utils';
+//#endregion Local Imports
+
+//#region Interface Imports
+import { I{{upperFileName}}} from '@Interfaces';
+//#endregion Interface Imports
+
+describe('{{upperFileName}} Service Helper Constructor', () => {
+	it('should module exist', async () => {
+		expect({{upperFileName}}Helper).toBeDefined();
+	});
+});
+
+describe('{{upperFileName}} service helpers', () => {
+	it('', async () => {
+		const params = {};
+
+		const result = await {{upperFileName}}Helper.methodName(DummyContext.getCall(params), params);
+
+		expect(result).toBeDefined();
+	});
+});
+`);
 
 // Create Package.json
 fs.writeFileSync('/package.json', `{ "pankod": { "projectType": "test", "plugins": ["styled"] } }`);
