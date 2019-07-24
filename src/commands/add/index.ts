5bd8b42cea61043507a3a4c8f692cdc5099c7f11
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-07-24 17:31:36 +0300

    Move validations to functions to reduce complexity

--- src/commands/add/index.ts
+++ src/commands/add/index.ts
@@ -31,20 +31,27 @@ export default class Add extends Command {
 
 	static usage = ['add Entity', 'add Service', 'add Page', 'add FunctionalComponent', 'add ClassComponent', 'add Plugin'];
 
+	validateProjectSupported = (projectType: string) => {
+		if (!supportedProjects.includes(projectType)) {
+			this.error(`The project ${projectType} isn't supported.`);
+		}
+	};
+
+	validateCommand = (entityType: string, projectType: string) => {
+		if (entityType && !supportedCommands[projectType].includes(entityType)) {
+			this.error(`Command "${entityType}" isn't supported by ${projectType} project.`);
+		}
+	};
+
 	async run() {
 		const { args: { entityType } }: IAddArgs = this.parse(Add);
 
 		const { projectType } = CommonHelper.getPankodConfig();
 
+		this.validateProjectSupported(projectType);
+		this.validateCommand(entityType, projectType);
+
 		let answers: ICommon.IAnswers = { fileName: '', fileType: '' };
-
-		if (!supportedProjects.includes(projectType)) {
-			this.error(`The project ${projectType} isn't supported.`);
-		}
-
-		if (entityType && !supportedCommands[projectType].includes(entityType)) {
-			this.error(`Command "${entityType}" isn't supported by ${projectType} project.`);
-		}
 
 		if (!entityType) {
 			answers = await inquirer.prompt(questions[projectType]);
@@ -54,7 +61,6 @@ export default class Add extends Command {
 
 		try {
 			await questionsHelper.default.showQuestions(answers.fileType || entityType);
-
 		} catch (error) {
 			this.error(error);
 		}
