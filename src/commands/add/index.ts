5313b602572a8286faa5a2ed733ec2fc17772e02
Furkan <furkan@pankod.com>
2019-11-12 16:55:27 +0300

    Refactored duplicate snippets.

--- src/commands/add/index.ts
+++ src/commands/add/index.ts
@@ -1,86 +1,60 @@
 import { Command } from '@oclif/command';
 
-import questions from '../../questions';
+import { getQuestionByProjectType, choices } from '../../questions';
 import { ICommon } from '../../Scripts/ICommon';
 import inquirer = require('inquirer');
 import { CommonHelper } from '../../Scripts/Common';
-import { IQuestionsHelper, ISupportedCommands } from '../../ITypes';
-
-interface IAddArgs {
-    args: {
-        entityType: string;
-    };
-}
-
-const supportedProjects: string[] = ['nextjs', 'moleculer', 'svelte', 'nextjs2'];
-
-const supportedCommands: ISupportedCommands = {
-    moleculer: ['Entity', 'Service'],
-    nextjs: ['Page', 'FunctionalComponent', 'ClassComponent'],
-    svelte: ['Component'],
-    nextjs2: ['Page', 'FunctionalComponent']
-};
+import { IQuestionsHelper } from '../../ITypes';
 
 export default class Add extends Command {
     static description = 'Add services, components and more...';
 
     static args = [
         {
-            name: 'entityType',
-            options: [
-                'Entity',
-                'Service',
-                'Page',
-                'FunctionalComponent',
-                'ClassComponent',
-                'Plugin',
-                'Component'
-            ]
+            name: 'element',
+            options: Object.values(choices)
+                .join()
+                .split(',')
         }
     ];
 
-    static usage = [
-        'add Entity',
-        'add Service',
-        'add Page',
-        'add FunctionalComponent',
-        'add ClassComponent',
-        'add Plugin'
-    ];
+    static usage = Object.values(choices)
+        .join()
+        .split(',')
+        .map(element => `add ${element}`);
 
     validateProjectSupported = (projectType: string) => {
-        console.log('asss');
-        if (!supportedProjects.includes(projectType)) {
+        if (!Object.keys(choices).includes(projectType)) {
             this.error(`The project ${projectType} isn't supported.`);
         }
     };
 
-    validateCommand = (entityType: string, projectType: string) => {
-        if (entityType && !supportedCommands[projectType].includes(entityType)) {
-            this.error(`Command "${entityType}" isn't supported by ${projectType} project.`);
+    validateCommand = (element: string, projectType: string) => {
+        if (element && !choices[projectType].includes(element)) {
+            this.error(`Command "${element}" isn't supported by ${projectType} project.`);
         }
     };
 
     async run() {
         const {
-            args: { entityType }
-        }: IAddArgs = this.parse(Add);
+            args: { element }
+        } = this.parse(Add);
 
         const { projectType } = CommonHelper.getPankodConfig();
 
         this.validateProjectSupported(projectType);
-        this.validateCommand(entityType, projectType);
+        this.validateCommand(element, projectType);
 
         let answers: ICommon.IAnswers = { fileName: '', fileType: '' };
 
-        if (!entityType) {
-            answers = await inquirer.prompt(questions[projectType]);
+        if (!element) {
+            answers = await inquirer.prompt(getQuestionByProjectType(projectType));
         }
 
         const questionsHelper: IQuestionsHelper = require(`../../Scripts/${projectType}/index`) as IQuestionsHelper;
 
         try {
-            await questionsHelper.default.showQuestions(answers.fileType || entityType);
+            await questionsHelper.default.showQuestions(answers.fileType || element);
         } catch (error) {
             this.error(error);
         }
