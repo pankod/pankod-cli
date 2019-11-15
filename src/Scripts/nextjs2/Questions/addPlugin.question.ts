c2ef0f37bad300ff4760cedc1aad87f76853ba1b
Furkan <furkan@pankod.com>
2019-11-15 16:49:03 +0300

    Discarded arbitrary enum.

--- src/Scripts/nextjs2/Questions/addPlugin.question.ts
+++ src/Scripts/nextjs2/Questions/addPlugin.question.ts
@@ -1,21 +1,20 @@
 import * as inquirer from 'inquirer';
-import { Plugins } from '../pluginsEnum';
 import { ICommon } from '../../ICommon';
 
 export const addPlugin: inquirer.ListQuestion<ICommon.IAnswers> = {
     message: 'What plugin do you want to add?',
     name: 'pluginType',
     type: 'list',
-    default: Plugins.styled,
+    default: 'styled',
     choices: [
         new inquirer.Separator(),
         {
             name: 'Styled Components',
-            value: Plugins.styled
+            value: 'styled'
         },
         {
             name: 'Sass',
-            value: Plugins.sass
+            value: 'sass'
         }
     ],
 };
--- src/Scripts/nextjs2/pluginsEnum.ts
+++
@@ -1,4 +0,0 @@
-export enum Plugins {
-    sass = 'sass',
-    styled = 'styled'
-}
