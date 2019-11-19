f1633832463d5b5b60f421b249ea50fa09c873e5
Furkan ACAR <furkan@acar.digital>
2019-11-20 01:39:23 +0300

    nextjs2 questions have been moved.

--- src/Scripts/nextjs2/Questions/connectStore.question.ts
+++
@@ -1,9 +0,0 @@
-import * as inquirer from 'inquirer';
-import { ICommon } from '../../ICommon';
-
-export const connectStore: inquirer.ConfirmQuestion<ICommon.IAnswers> = {
-    message: 'Do you want to connect store ?',
-    name: 'isConnectStore',
-    type: 'confirm',
-    default: false
-};
