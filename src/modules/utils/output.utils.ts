88c577efe1ecd1923a4c716dede5d4c92b60a951
Furkan <furkan@pankod.com>
2019-11-20 17:44:14 +0300

    Import of `chalk` updated to use default export.

--- src/modules/utils/output.utils.ts
+++ src/modules/utils/output.utils.ts
@@ -1,16 +1,18 @@
 // #region Global Imports
-import { red, bold } from 'chalk';
+import chalk from 'chalk';
 // #endregion Global Imports
 
 // TODO: Create a better renderer
 export const renderMessage = {
     invalidProject: (project: string) => {
-        return `${bold(project)} is NOT a supported project!`;
+        return `${chalk.bold(project)} is NOT a supported project!`;
     },
 
     invalidElement: (project: string, element: string) => {
-        const [p, e] = [project, element].map(bold);
+        const [p, e] = [project, element].map(w => chalk.bold(w));
 
-        return red(`${p} projects have no element called ${e} to be created!`);
+        return chalk.red(
+            `${p} projects have no element called ${e} to be created!`
+        );
     }
 };
