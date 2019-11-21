86a9379752c50ef59baec51abad0012bc7750a45
Furkan <furkan@pankod.com>
2019-11-21 16:35:40 +0300

    Ignored `__temp__`. Initial test with snapshot.

--- src/modules/element-factory/workbenches/operations/addToIndex.operation.ts
+++ src/modules/element-factory/workbenches/operations/addToIndex.operation.ts
@@ -10,6 +10,18 @@ import { ICommon } from '../../../typings';
 // #endregion Local Imports
 
 export const addToIndex = (params: ICommon.IAddIndex): void => {
+    const folders = params.dirPath.split('/');
+
+    // * <Failsafe>
+    folders.reduce((acc, curr) => {
+        const target = path.join(...acc, curr);
+
+        if (!fs.existsSync(target)) fs.mkdirSync(target);
+
+        return [...acc, curr];
+    }, ['.']);
+    // * </Failsafe>
+
     fs.appendFileSync(
         path.resolve('', params.dirPath),
         `${params.getFileContent()}`
--- src/modules/element-factory/workbenches/operations/tests/addToIndex.spec.ts
+++ src/modules/element-factory/workbenches/operations/tests/addToIndex.spec.ts
@@ -1,10 +1,30 @@
-import { addToIndex } from '../addToIndex.operation';
+import * as path from "path"
+import * as fs from 'fs';
+
 import { operations } from '../../../../utils';
+import { addToIndex } from '../addToIndex.operation';
+import { getTemplate } from '../getTemplate.operation';
+import { ICommon } from '../../../../typings';
 
 describe(operations, () => {
     describe('addToIndex.operation', () => {
+        const params: ICommon.IAddIndex = {
+            dirPath: `__temp__/elements/index.ts`,
+            getFileContent: () => {
+                return getTemplate(
+                    path.resolve('./lib/Templates/nextjs/Components/index.mustache'),
+                    { fileName: 'Test' }
+                );
+            },
+            message: 'Entity added to index.ts.'
+        };
+
+        addToIndex(params);
+
+        const created = String(fs.readFileSync(params.dirPath));
+
         test('should pass', () => {
-            expect(true).toBeTruthy();
+            expect(created).toMatchSnapshot();
         });
     });
 });
