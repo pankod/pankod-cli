e8a3662d108dddf6b17064276d8377a1611af743
Furkan <furkan@pankod.com>
2019-11-21 12:39:47 +0300

    Added colored titles for test descriptions.

--- src/modules/utils/output.utils.ts
+++ src/modules/utils/output.utils.ts
@@ -2,17 +2,53 @@
 import chalk from 'chalk';
 // #endregion Global Imports
 
+type Color = 'bgBlue' | 'bgGreen' | 'bgCyan' | 'bgMagenta';
+
+const { bgBlue, bgGreen, bgCyan, bgMagenta, red, bold, italic } = chalk;
+
 // TODO: Create a better renderer
 export const renderMessage = {
     invalidProject: (project: string) => {
-        return `${chalk.bold(project)} is NOT a supported project!`;
+        return red(`${bold(project)} is NOT a supported project!`);
     },
 
     invalidElement: (project: string, element: string) => {
-        const [p, e] = [project, element].map(w => chalk.bold(w));
+        const [p, e] = [project, element].map(w => bold(w));
 
-        return chalk.red(
-            `${p} projects have no element called ${e} to be created!`
-        );
+        return red(`${p} projects have no element called ${e} to be created!`);
     }
 };
+
+export const renderDescription = (
+    module: string,
+    description: string,
+    bgColor: Color = 'bgBlue'
+) => {
+    return `${chalk[bgColor](bold('[operations]'))} -- ${italic(description)}`;
+};
+
+// * Colored Test Descriptions
+export const operations = renderDescription('[operations]', 'fs manipulations');
+
+// * Tasks
+export const tasks = renderDescription('[tasks]', 'scaffolding');
+
+export const tasksNextjs2 = renderDescription(
+    '[tasks > nextjs2]',
+    'scaffolding elements',
+);
+export const tasksNextjs = renderDescription(
+    '[tasks > nextjs]',
+    'scaffolding elements',
+    'bgCyan'
+);
+export const tasksSvelte = renderDescription(
+    '[tasks > svelte]',
+    'scaffolding elements',
+    'bgGreen'
+);
+export const tasksMoleculer = renderDescription(
+    '[tasks > moleculer]',
+    'scaffolding elements',
+    'bgMagenta'
+);
