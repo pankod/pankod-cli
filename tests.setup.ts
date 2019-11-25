e976bafd33cf64fc934d07386b212e550425cc73
Furkan <furkan@pankod.com>
2019-11-25 09:38:55 +0300

    Added comment for better developer experience.

--- tests.setup.ts
+++ tests.setup.ts
@@ -1,1 +1,5 @@
-jest.mock('fs', () => require('memfs'));
+// ! Uncomment line below to
+// ! work in-memory instead of disk
+// ! also to make tests run in CI environments.
+
+// jest.mock('fs', () => require('memfs'));
