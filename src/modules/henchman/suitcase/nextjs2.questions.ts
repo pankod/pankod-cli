2b0be4b19e826f322921941713656cb07b2975c5
Furkan <furkan@pankod.com>
2019-11-26 17:57:20 +0300

    Progress addition of page and component implementation options.

--- src/modules/henchman/suitcase/nextjs2.questions.ts
+++ src/modules/henchman/suitcase/nextjs2.questions.ts
@@ -3,6 +3,22 @@ import * as questions from './questions/nextjs2';
 import { INextjs2Questions } from '../../typings';
 // #endregion Local Imports
 
+const componentQuestions = [
+    questions.enterComponentName,
+    questions.addStyle,
+    questions.connectStore,
+    questions.isHaveReducer
+];
+
+const pageQuestions = [
+    questions.enterPageName,
+    questions.customPath,
+    questions.enterRouteName,
+    questions.connectStore,
+    questions.isHaveReducer,
+    questions.addStyle
+];
+
 export const nextjs2: INextjs2Questions = {
     ClassComponent: [
         questions.enterComponentName,
@@ -15,6 +31,16 @@ export const nextjs2: INextjs2Questions = {
         questions.enterComponentName,
         questions.addStyle
     ],
+
+    // Component: [
+    //     questions.implementation, // ? isClass | isFunction
+    //     ...componentQuestions
+    // ],
+
+    // Page: [
+    //     questions.implementation, // ? isClass | isFunction
+    //     ...pageQuestions
+    // ],
 
     Page: [
         questions.enterPageName,
