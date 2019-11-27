2f426b7b5494b1b6a883e1764ece809a6cd8f828
Furkan <furkan@pankod.com>
2019-11-27 13:06:28 +0300

    Added implementation options for both Page and Component.

--- src/modules/henchman/suitcase/nextjs2.questions.ts
+++ src/modules/henchman/suitcase/nextjs2.questions.ts
@@ -3,46 +3,9 @@ import * as questions from './questions/nextjs2';
 import { INextjs2Questions } from '../../typings';
 // #endregion Local Imports
 
-const componentQuestions = [
-    questions.enterComponentName,
-    questions.addStyle,
-    questions.connectStore,
-    questions.isHaveReducer
-];
-
-const pageQuestions = [
-    questions.enterPageName,
-    questions.customPath,
-    questions.enterRouteName,
-    questions.connectStore,
-    questions.isHaveReducer,
-    questions.addStyle
-];
-
 export const nextjs2: INextjs2Questions = {
-    ClassComponent: [
-        questions.enterComponentName,
-        questions.addStyle,
-        questions.connectStore,
-        questions.isHaveReducer
-    ],
-
-    FunctionalComponent: [
-        questions.enterComponentName,
-        questions.addStyle
-    ],
-
-    // Component: [
-    //     questions.implementation, // ? isClass | isFunction
-    //     ...componentQuestions
-    // ],
-
-    // Page: [
-    //     questions.implementation, // ? isClass | isFunction
-    //     ...pageQuestions
-    // ],
-
     Page: [
+        questions.implementation,
         questions.enterPageName,
         questions.customPath,
         questions.enterRouteName,
@@ -51,7 +14,15 @@ export const nextjs2: INextjs2Questions = {
         questions.addStyle
     ],
 
-    Plugin: [
-        questions.addPlugin
+    Component: [
+        questions.implementation,
+        questions.enterComponentName,
+        questions.addStyle,
+        questions.connectStore,
+        questions.isHaveReducer
     ]
+
+    // Plugin: [
+    //     questions.addPlugin
+    // ]
 };
